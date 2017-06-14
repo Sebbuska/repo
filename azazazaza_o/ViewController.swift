bb1719d5dae5b9a59475e85cb519f4bbbdd7c3ba
Oleksiy Gerasimov <oleksiy.gerasimov@thinkmobiles.com>
2017-06-14 15:22:33 +0300

    Initial Commit

---
+++ azazazaza_o/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  azazazaza_o
+//
+//  Created by Sergey Sheba on 6/14/17.
+//  Copyright © 2017 Sebbuska. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
